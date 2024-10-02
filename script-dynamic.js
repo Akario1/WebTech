6e5b02aede26e09ab4b23b518603b0b7b39a9f23
Akario1 <155559752+Akario1@users.noreply.github.com>
2024-10-02 16:11:40 +0200

    Create script-dynamic.js

---
+++ script-dynamic.js
@@ -0,0 +1,40 @@
+let page = 1
+
+function fetchData(){
+    const url = `https://jsonplaceholder.typicode.com/posts?_page=${page}`;
+
+    fetch(url)
+    .then(response => {
+        return response.json();
+    })
+
+    .then(data => {
+        const container = document.getElementById('post-container');
+
+        if (data.length > 0) {
+            data.forEach(post => {
+                const postDiv = document.createElement('div');
+                postDiv.classList.add('post');
+                postDiv.innerHTML = `<h2>${post.title}</h2><p>${post.body}</p>`;
+                container.appendChild(postDiv);
+            })
+        };
+        page++;
+    })
+}
+
+/**
+ * adds an  event listener to the current windows
+ * when the user scrolls further than the height of the page
+ * fetchData() is called
+ */
+window.addEventListener('scroll', ()=> {
+    if(window.scrollY + window.innerHeight >=
+        document.documentElement.scrollHeight)
+        {
+            fetchData();
+    }
+});
+
+// initial data
+fetchData();
