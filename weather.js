e0c5a8e88b246569492b528f62314d6f69bb8ed0
Akario1 <155559752+Akario1@users.noreply.github.com>
2024-10-05 17:04:20 +0200

    Update to dynamically fetch all locations weather.js

--- weather.js
+++ weather.js
@@ -6,52 +6,46 @@ const locations = [
     {city: 'Grimstad', latitude: 58.3405, longitude: 8.5934}
 ];
 
-function fetchWeather(location){
+
+
+const container = document.getElementById('weather-container');
+
+function clearDocument() {
+    container.innerHTML = ``;
+}
+
+function getWeather(location){
     const url = `https://api.open-meteo.com/v1/forecast?latitude=${location.latitude}&longitude=${location.longitude}&current_weather=true`
+    
     fetch(url)
     .then(response => {
          return response.json();
     })
+
     .then(data => {
+        
         const currentWeather = data.current_weather;
-        const container = document.getElementById('weather-container');
-        
-        const cityDiv = document.createElement('div');
-        cityDiv.classList.add('city-weather');
-        
-        cityDiv.innerHTTML = `
+        const weatherDiv = document.createElement('div');
+
+        weatherDiv.classList.add('city-weather')
+        weatherDiv.innerHTML = `
             <h2>${location.city}</h2>
-            <p>Last Updated ${currentWeather.time} GMT</p>
-            <p>Temperature: ${currentWeather.temp} °C</p>
-            <p>Wind speed: ${currentWeather.windspeed} km/h</p>
+            <p>Last updated: ${currentWeather.time}GMT</p>
+            <p>Temperature: ${currentWeather.temperature}°C</p>
+            <p>Windspeed: ${currentWeather.windspeed}km/h</p>
         `;
-        container.appendChild(cityDiv);
-    })
-    }
-    
-    /** 
-    url1 = `https://api.open-meteo.com/v1/forecast?latitude=60.7957&longitude=10.6915&current_weather=true`
-    fetch(url1)
-    .then(response => {
-        return response.json()
-    })
 
-    .then(data => {
-        const currentWeather = data.current_weather;
-        document.getElementById('temp-g').textContent = `Temperature: ${currentWeather.temperature}°C`
-        document.getElementById('wind-g').textContent = `Windspeed: ${currentWeather.windspeed} km/h`
-    
-    })
-    */
-
-
-
-
-function fetchAll(){
-    locations.forEach(location => {
-        fetchWeather(location);
+        container.appendChild(weatherDiv);
     })
 }
 
+function fetchAll(){
+    clearDocument();
+    locations.forEach(location => {
+        getWeather(location);
+    });
+}
+
 fetchAll();
-setInterval(fetchAll, 3600000);
+
+setInterval(fetchAll, 900000)
